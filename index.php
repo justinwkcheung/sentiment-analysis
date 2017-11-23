6ad905dc2ee6208feeacabc385aa94c46563ee44
Justin Cheung <justinwkcheung@github.com>
2017-11-22 23:51:53 -0500

    Establish way to map values of first row as the keys of the rest of the rows

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text=I%20really%20love%20your%20APIs&token=50e13e6b06bb4b0c9a233b11acb5687e",
+  CURLOPT_URL => "https://api.dandelion.eu/datatxt/sent/v1/?lang=en&url=https://twitter.com/realDonaldTrump/status/797034721075228672&token=50e13e6b06bb4b0c9a233b11acb5687e",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
@@ -23,5 +23,18 @@ $err = curl_error($curl);
 
 curl_close($curl);
 var_dump($response);
+
+?>
+
+<?php
+
+// $file = fopen("https://www.crowdbabble.com/wp-content/uploads/2016/11/Crowdbabble_Social-Media-Analytics_Twitter-Download_Donald-Trump_7375-Tweets.csv","r");
+//
+// while(! feof($file))
+// {
+// print_r(fgetcsv($file));
+// }
+//
+// fclose($file);
 
 ?>
--- upload.php
+++ upload.php
@@ -1,8 +1,20 @@
 <?php
-var_dump('hello');
+
+ini_set("auto_detect_line_endings", true);
 
 $tmpName = $_FILES['csv']['tmp_name'];
 $csvAsArray = array_map('str_getcsv', file($tmpName));
+// array_map('str_getcsv', file('data.csv' , FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES));
+
+//First I want to grab the values from the first row to be the keys of the next rows
+$keys = [];
+foreach ($csvAsArray[0] as $titleKeys=>$titleValues) {
+  array_push($keys, $titleValues);
+}
+
+foreach ($csvAsArray as $i=>$row) {
+    $csvAsArray[$i] = array_combine($keys, $row);
+}
 
 if (!empty($_FILES["csv"])) {
   $myFile = $_FILES["csv"];
@@ -12,15 +24,15 @@ if($myFile['error'] > 0){
   die('<div> An error occurred while uploading the file </div>');
 }
 
-$csv_mimetypes = array(
-    'text/csv',
-    'text/plain',
-    'application/csv',
-);
-if (!in_array($myFile['type'], $csv_mimetypes)) {
-  die('<div> An error occurred while uploading the file as CSV </div>');
-}
+// $csv_mimetypes = array(
+//     'text/csv',
+//     'text/plain',
+//     'application/csv',
+// );
+// if (!in_array($myFile['type'], $csv_mimetypes)) {
+//   die('<div> An error occurred while uploading the file </div>');
+// }
 
-var_dump($myFile);
-var_dump($csvAsArray);
+// var_dump($_FILES['csv']);
+// var_dump($csvAsArray);
 ?>
